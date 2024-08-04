016c8a81b13a812c95c3bb0fc3767ad3e0c61e72
Sergiy Dolgov <info@devmax.pro>
2024-08-04 15:57:53 -0400

    make build

---
+++ index.9e5c6b9a.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("#thumbs"),e=document.querySelector("#largeImg");t.addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");e.setAttribute("src",r.href),e.setAttribute("alt",r.title)});
+//# sourceMappingURL=index.9e5c6b9a.js.map
